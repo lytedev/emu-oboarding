fa35c81e0da8d38bc03ee7dcab640c8ef46e738e
Daniel Flanagan <daniel@lytedev.io>
2016-10-22 03:28:09 -0500

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+#app
+  hello
+</template>
+
+<script lang="coffee">
+require './styles/_variables.styl'
+
+Hello = require './components/Hello.vue'
+
+module.exports =
+  name: 'app'
+  components:
+    Hello: Hello
+</script>
+
+<style lang="stylus">
+@import url('https://cdnjs.cloudflare.com/ajax/libs/normalize/5.0.0/normalize.min.css')
+@import url('https://fonts.googleapis.com/css?family=Titillium+Web')
+@import './styles/_variables.styl'
+
+#app
+  font-family 'Titillium Web', Helvetica, Arial, sans-serif
+  -webkit-font-smoothing antialiased
+  -moz-osx-font-smoothing grayscale
+  text-align center
+  color $page-foreground-color
+  font-size 200%
+</style>
---
+++ src/main.js
@@ -0,0 +1,20 @@
+import MainStyles from './styles/main.styl'
+
+import Vue from 'vue'
+import VueRouter from 'vue-router'
+import App from './App'
+
+Vue.use(VueRouter)
+
+var routes = [
+	{ path: '/', component: App }
+]
+
+var router = new VueRouter({
+	routes
+})
+
+/* eslint-disable no-new */
+var app = new Vue({
+	router
+}).$mount('#app-mount')
